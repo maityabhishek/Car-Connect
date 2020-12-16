1370999fc2f858d1bc2f2dfcba1db885f29b14f7
lalitjchaudhari <lalitjchaudhari@gmail.com>
2020-12-16 09:42:08 +0530

    Added the swiper to show different notifications

---
+++ store/data/Notifications.js
@@ -0,0 +1,14 @@
+export const notifications = [
+    {
+        eventtype: "regular",
+        desc: "Cooolant temprature exceeded",
+        vin: "1234",
+        user: "Amol"
+    },
+    {
+        eventtype: "alert",
+        desc: "Low fuel level",
+        vin: "1234",
+        user: "Amol"
+    }
+]
